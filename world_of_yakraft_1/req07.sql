aceddc1a2bc7356ec7efda29f2fb56c5f5e4e88d
Mickael Chau <mickaelchau4@gmail.com>
2022-01-04 20:23:41 +0100

    all requests transaction & kuro level up at place

--- world_of_yakraft_1/req07.sql
+++ world_of_yakraft_1/req07.sql
@@ -1,16 +1,23 @@
+BEGIN;
 UPDATE character
 SET level=15
 WHERE name='Tilon';
+END;
 
-UPDATE character
-SET max_health=level*10
-WHERE blessed=0;
-
-UPDATE character
-SET max_health=(level+1)*10
-WHERE blessed=1;
-
+BEGIN;
 UPDATE character
 SET level=level+1
 WHERE name='Kuro';
+END;
+
+BEGIN;
+UPDATE character
+SET max_health=level*10
+WHERE blessed=0;
+END;
+BEGIN;
+UPDATE character
+SET max_health=(level+1)*10
+WHERE blessed=1;
+END;
 
