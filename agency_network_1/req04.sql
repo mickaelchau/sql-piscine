0bc441b6c182992c5d3d412f4f8d0ffdc16b557f
Mickael Chau <mickaelchau4@gmail.com>
2022-01-04 15:35:57 +0100

    2

--- agency_network_1/req04.sql
+++ agency_network_1/req04.sql
@@ -1,2 +1,4 @@
-SELECT country, count(*) as count FROM destination GROUP BY country ORDER BY count DESC;
+SELECT count(*),my_db.country 
+FROM (SELECT country FROM destination GROUP BY city,country) as my_db 
+GROUP BY my_db.country ORDER BY count DESC;
 
