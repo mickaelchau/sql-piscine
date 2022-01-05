e5abf95bc2507b3ba0a29d953be3935b9383ff1f
Mickael Chau <mickaelchau4@gmail.com>
2022-01-05 16:56:30 +0100

    modif view_average_period

--- planet_base/views.sql
+++ planet_base/views.sql
@@ -12,11 +12,13 @@ FROM planet LEFT OUTER JOIN (SELECT planet.name,count(*) FROM satellite
 ON planet.name=data.name ORDER BY "number_of_satellites", planet.name;
 
 CREATE OR REPLACE VIEW view_average_period AS
-SELECT data.star, round((sum(period)::float/count(*))::numeric,2)
-FROM (SELECT star, planet.name, period
+SELECT data.system, round((sum(period)::float/count(*))::numeric,2) AS average_period
+FROM (SELECT ps.name as system, planet.name, period
     FROM planetary_system AS ps 
     INNER JOIN planet ON ps.id=planet.id_system) AS data 
-GROUP BY data.star;
+GROUP BY data.system
+ORDER BY average_period, data.system;
+
 
 CREATE OR REPLACE VIEW view_biggest_entities AS
 (SELECT 'satellite' AS type, ps.name AS system, satellite.name, satellite.radius FROM satellite INNER JOIN planet ON id_planet=planet.id INNER JOIN planetary_system AS ps ON planet.id_system=ps.id) UNION (SELECT 'planet', ps.name, planet.name,radius FROM planet INNER JOIN planetary_system AS ps ON ps.id=planet.id_system) ORDER BY radius DESC, name LIMIT 10;
