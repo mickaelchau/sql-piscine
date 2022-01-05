48fc89194583b28f448d17c50eb7175d7d66ef23
Mickael Chau <mickaelchau4@gmail.com>
2022-01-05 12:29:30 +0100

    2 first proc

---
+++ planet_base/procedures.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE FUNCTION space_travel(origin INT, destination INT, quantity BIGINT)
+RETURNS boolean AS
+$$
+BEGIN
+    IF (((SELECT population FROM planet WHERE planet.id=origin) - quantity) < 0) THEN 
+        RETURN false;
+    END IF;
+    UPDATE planet
+    SET population=population-quantity 
+    WHERE id=origin;
+    UPDATE planet
+    SET population=population+quantity 
+    WHERE id=destination;
+    RETURN true;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION list_satellite_inf_750(syst INT)
+RETURNS TABLE(satellite VARCHAR(32), planet VARCHAR(32), radius INT) AS
+$$
+BEGIN
+    RETURN QUERY(SELECT satellite.name, planet.name, satellite.radius FROM satellite 
+        INNER JOIN planet ON satellite.id_planet=planet.id 
+        INNER JOIN planetary_system ON planet.id_system=planetary_system.id 
+        WHERE (planet.id_system=planetary_system.id AND satellite.radius<=750)
+        ORDER BY planet.name, satellite.radius DESC, satellite.name);
+END;
+$$ LANGUAGE plpgsql;
+
