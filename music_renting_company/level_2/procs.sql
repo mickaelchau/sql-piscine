f632e7c9dbe8054996eefdf23cf263497a980f04
Mickael Chau <mickaelchau4@gmail.com>
2022-01-05 19:44:48 +0100

    rent_album pass all given tests

--- music_renting_company/level_2/procs.sql
+++ music_renting_company/level_2/procs.sql
@@ -6,29 +6,31 @@ DECLARE
     customer_id INT;
     album_id INT;
     matching_rent_id INT;
-    stock INT;
+    stock_num INT;
+    stock_id INT;
 BEGIN
     customer_id := (SELECT * FROM COALESCE((SELECT id FROM customer                        
         WHERE mail=email), -1)); 
     album_id := (SELECT * FROM COALESCE((SELECT id FROM album AS a                       
         WHERE a.name=album), -1));
-    stock := (SELECT s.stock FROM stock AS s);
+    stock_num := (SELECT s.stock FROM stock AS s WHERE s.alb_id=album_id);
+    stock_id := (SELECT id FROM stock AS s WHERE s.alb_id=album_id);
     matching_rent_id := (SELECT * FROM COALESCE((
             SELECT id FROM rent 
-            WHERE (pers_id=customer_id AND stock_id=album_id)), -1)); 
+            WHERE (rent.prs_id=customer_id AND rent.stock_id=album_id)), -1)); 
     IF (customer_id = -1) THEN
         RETURN false;
     ELSIF (album_id = -1) THEN
         RETURN false;
     ELSIF (matching_rent_id <> -1) THEN
         RETURN false;
-    ELSIF (stock <= 0) THEN
+    ELSIF (stock_num <= 0) THEN
         RETURN false;
     END IF;
-    UPDATE stock AS s
-        SET s.stock=s.stock-1
-        WHERE s.id=album_id;
-    INSERT INTO rent VALUES(default, customer_id, begin_date, now());
+    UPDATE stock
+        SET stock=stock-1
+        WHERE alb_id=album_id;
+    INSERT INTO rent VALUES(default, stock_id, customer_id, begin_date, now());
     RETURN true;
 END; 
 $$ LANGUAGE plpgsql;
