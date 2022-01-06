391c93759d107b5e753aefc7b126b73fa9e28dad
Mickael Chau <mickaelchau4@gmail.com>
2022-01-06 21:01:54 +0100

    2nd view corrections and 1st try for the 3th

--- music_renting_company/level_2/views.sql
+++ music_renting_company/level_2/views.sql
@@ -24,7 +24,7 @@ END
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE VIEW view_customers AS 
-SELECT customer.mail, customer.name, COALESCE(total_rents,0) as total_rents, 
+SELECT customer.mail as email, customer.name, COALESCE(total_rents,0) as total_rents, 
 COALESCE(current_rents,0) as current_rents FROM customer 
 LEFT OUTER JOIN (
     SELECT mail as email, name, count(*) AS total_rents, 
@@ -36,3 +36,10 @@ ON customer.mail=data.email ORDER BY customer.mail;
 CREATE OR REPLACE VIEW view_stocks AS 
 SELECT name AS album,stock FROM stock INNER JOIN album ON alb_id=album.id 
 ORDER BY name;
+
+CREATE OR REPLACE VIEW view_rents AS 
+SELECT customer.mail AS email, album.name AS album, 
+begin_date AS begin, end_date as end FROM rent 
+    INNER JOIN customer ON rent.prs_id=customer.id
+    INNER JOIN stock ON rent.stock_id=stock.id
+    INNER JOIN album ON stock.alb_id=album.id;
