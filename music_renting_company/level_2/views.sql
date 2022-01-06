1c4debeef37c384a2b3254f93d1c7c0a2981615e
Mickael Chau <mickaelchau4@gmail.com>
2022-01-06 18:28:17 +0100

    customers view

---
+++ music_renting_company/level_2/views.sql
@@ -0,0 +1,32 @@
+DROP VIEW IF EXISTS view_customers;view_customers
+
+CREATE OR REPLACE FUNCTION get_customers(client_id INT)
+RETURNS INT AS
+$$
+BEGIN
+    IF ((SELECT prs_id from rent WHERE ((rent.end_date IS null) 
+        AND (prs_id=client_id))) IS NOT null) THEN
+        RETURN 1;
+    END IF;
+    RETURN 0;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION print_customers()
+RETURNS INT AS
+$$
+DECLARE 
+    id INT;
+    current_rents INT; 
+BEGIN
+    current_rents := get_customers(id);
+END
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE VIEW view_customers AS 
+SELECT mail as email, name, count(*) AS total_rents, 
+get_customers(rent.prs_id) AS "current_rents"
+FROM rent INNER JOIN customer ON rent.prs_id=customer.id 
+GROUP by email, name, customer.id, rent.prs_id;
+
+
